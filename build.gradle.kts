d6b5c7e8f55cf7ee0d090009e78916c6e3ee9b00
Neb <nebatzel@gmail.com>
2026-02-15 23:08:01 -0500

    Added laptop monitoring system

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    application
 }
 
 group = "org.example"
@@ -11,6 +12,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.prometheus:simpleclient:0.16.0")
+    implementation("io.prometheus:simpleclient_httpserver:0.16.0")
+    implementation("com.github.oshi:oshi-core:6.4.0")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,50 @@
+import io.prometheus.client.Gauge
+import io.prometheus.client.exporter.HTTPServer
+import oshi.SystemInfo
+import oshi.hardware.CentralProcessor
+import oshi.hardware.GlobalMemory
 
-fun main () {
+fun main() {
 
-    println("hello world")
+    // Prometheus metrics
+    val cpuUsage = Gauge.build()
+        .name("laptop_cpu_usage_percent")
+        .help("Current CPU usage percentage")
+        .register()
 
+    val memoryUsed = Gauge.build()
+        .name("laptop_memory_used_bytes")
+        .help("Memory used in bytes")
+        .register()
+
+    val threadCount = Gauge.build()
+        .name("jvm_thread_count")
+        .help("Current JVM thread count")
+        .register()
+
+    // Start HTTP server for Prometheus to scrape
+    HTTPServer(9091)
+
+    val systemInfo = SystemInfo()
+    val processor: CentralProcessor = systemInfo.hardware.processor
+    val memory: GlobalMemory = systemInfo.hardware.memory
+
+    println("Metrics server started at http://localhost:9091/metrics")
+
+    var previousTicks = processor.systemCpuLoadTicks
+
+    while (true) {
+
+        val cpu = processor.getSystemCpuLoadBetweenTicks(previousTicks) * 100
+        previousTicks = processor.systemCpuLoadTicks
+
+        val usedMem = memory.total - memory.available
+        val threads = Thread.activeCount()
+
+        cpuUsage.set(cpu)
+        memoryUsed.set(usedMem.toDouble())
+        threadCount.set(threads.toDouble())
+
+        Thread.sleep(5000)
+    }
 }
